8a781bcb2524da8f887f12a55679fb9ecb41319c
shreyasbangera <shreyasybangera1@gmail.com>
2023-02-11 20:05:59 +0530

    update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,6 @@ import { motion } from 'framer-motion'
 const Hero = () => {
   return (
     <motion.div
-    layout
     initial={{ opacity: 0, scale: 0.5 }}
     animate={{ opacity: 1, scale: 1 }}
     transition={{ duration: 0.5 }}
